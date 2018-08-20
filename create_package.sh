d7b540cd5c7e10a52fbaaa4893fafd9872c62864
DarkCaster <dark.caster@outlook.com>
2018-08-21 00:49:23 +0300

    create_package.sh updated, enable atmega2560 build

--- create_package.sh
+++ create_package.sh
@@ -96,7 +96,7 @@ dos2unix -q -r -n "$curdir/src/external/pins_arduino.h" "$temp_dir/$package_name
 #build optiboot for supported platforms
 log "building bootloaders"
 build_optiboot 20000000L 250000 "atmega328 atmega328p atmega328pb"
-build_optiboot 16000000L 250000 "atmega328 atmega328p atmega328pb atmega1284p"
+build_optiboot 16000000L 250000 "atmega328 atmega328p atmega328pb atmega1284p atmega2560"
 build_optiboot 8000000L 19200 "atmega328 atmega328p atmega328pb atmega1284p"
 build_optiboot 1000000L 9600 "atmega328 atmega328p atmega328pb"
 
